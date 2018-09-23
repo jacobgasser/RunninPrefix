dff75cbf3465c157de53824d628ddf86a3ddf396
Jacob Gasser <jacobgasser3@gmail.com>
2018-09-23 19:09:26 -0400

    singltonss

--- src/com/runninplugins/prefix/Tag.java
+++ src/com/runninplugins/prefix/Tag.java
@@ -1,8 +1,37 @@
 package com.runninplugins.prefix;
 
-public class Tag {
-    private boolean isHoverable;
-    public Tag() {
+import org.bukkit.ChatColor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tag {
+    private static List<Tag> tags = new ArrayList<>();
+    private boolean isHoverable;
+    private String hoverContent;
+    private String content;
+    private String tagName;
+    public Tag(String tagName, String content, boolean isHoverable, String hoverContent) {
+        this.tagName = tagName;
+        this.content = content;
+        this.isHoverable = isHoverable;
+        this.hoverContent = hoverContent;
+        tags.add(this);
+    }
+    public String getTagName() {
+        return tagName;
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public static Tag getTagByName(String name) {
+        for(Tag tag : tags) {
+            if(tag.getTagName().equals(name)) {
+                return tag;
+            }
+        }
+        return null;
     }
 }
+
+
