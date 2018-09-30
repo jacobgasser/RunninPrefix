22954003a6a56bef0e0368f15342a5f8db4f1a02
Harry <dawnserverowner@gmail.com>
2018-09-30 09:16:35 +0100

    Command registrar finalized.

--- src/main/java/com/runninplugins/prefix/commands/CommandRegistrar.java
+++ src/main/java/com/runninplugins/prefix/commands/CommandRegistrar.java
@@ -3,12 +3,16 @@ package com.runninplugins.prefix.commands;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.defaults.BukkitCommand;
+import org.reflections.Reflections;
 
 import java.lang.reflect.Field;
+import java.util.Set;
 
 public class CommandRegistrar {
 
-    public void registerPlugin(BukkitCommand command){
+
+
+    public void registerCommand(BukkitCommand command){
         try {
             final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
 
@@ -21,6 +25,19 @@ public class CommandRegistrar {
         }
     }
 
+    public void addCommands(){
+        Reflections reflections = new Reflections("com.runninplugins.prefix.commands");
+        Set<Class<? extends BukkitCommand>> commands = reflections.getSubTypesOf(BukkitCommand.class);
+        try {
+            for(Class<? extends BukkitCommand> clazz : commands){
+                    registerCommand((BukkitCommand) clazz.newInstance());
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
